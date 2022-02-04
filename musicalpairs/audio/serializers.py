b4244f8ce2ce6d499c04b36a7708421b1996900f
james <jamesbowen5@hotmail.com>
2022-02-04 19:31:45 +0000

    clearing

--- musicalpairs/audio/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Audio_store
-
-
-class Audio_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Audio_store
-        fields = ('word', 'allow_mumble')
