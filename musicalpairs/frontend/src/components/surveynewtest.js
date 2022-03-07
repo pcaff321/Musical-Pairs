5e17fca96b68830fefa8b8a7c744dbe36899d117
Paul Caffrey <58259624+pcaff321@users.noreply.github.com>
2022-03-07 15:08:53 +0000

    Fixed some CSS issues with React

--- musicalpairs/frontend/src/components/surveynewtest.js
+++ musicalpairs/frontend/src/components/surveynewtest.js
@@ -257,7 +257,10 @@ export default function Survey(props) {
             } else if (roomData.round_list[i][0] == "audio") {
                 initial_components[i] = (
                     <div>
-                        <p style={{ textAlign: "center" }}>Please Close Your Eyes</p>
+                        <Grid item xs={12} align="center">
+                            <img src="/media/CloseYourEyes.png" style={{ height: "80%", width: "80%", 'object-fit': "contain", top: "1em", position: "relative" }}></img>
+                        </Grid><br></br><br></br>
+                        <h2 style={{ textAlign: "center" }}><strong>Please Close Your Eyes </strong></h2>
                         <audio controls autoPlay hidden src={roomData.round_list[i][4]}>
                             <source src={roomData.round_list[i][4]} type="audio/wav"></source>
                         </audio>
@@ -388,7 +391,7 @@ export default function Survey(props) {
             } else if (roomData.round_list[i][0] == "image") {
                     initial_components[i] = (
                         <Grid item xs={12} align="center">
-                            <img src={roomData.round_list[i][1]} style={{ top: "1em", position: "relative" }}></img>
+                            <img src={roomData.round_list[i][1]} style={{ height: "60%", width: "80%", 'object-fit': "contain", top: "1em", position: "relative" }}></img>
                         </Grid>
                     )
             } else {
