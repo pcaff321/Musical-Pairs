9d02d5494b5fff3dedcbae1698a5b7ff3f93e1c4
james <jamesbowen5@hotmail.com>
2022-03-08 15:03:06 +0000

    Update surveynewtest.js

--- musicalpairs/frontend/src/components/surveynewtest.js
+++ musicalpairs/frontend/src/components/surveynewtest.js
@@ -286,6 +286,7 @@ export default function Survey(props) {
                 } else if (roomData.round_list[i][2] == "Agree") {
                     question = (
                         <RadioGroup id="agree" row defaultValue="3" onChange={handleAnswerChange} style={{ display: "block" }}>
+                            <p>On a scale of 1-5, how much do you agree with the preceding statement?</p>
                             <FormControlLabel 
                                 value="1" control={<Radio color="primary" />} 
                                 label="1" labelPlacement="bottom"
