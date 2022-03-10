c08d9d62e85d64b6ce3bbccfdd8f4f47c8e55695
milena <milenakalita@outlook.com>
2022-03-10 16:01:01 +0000

    bug fix

--- musicalpairs/audio/views.py
+++ musicalpairs/audio/views.py
@@ -1151,7 +1151,7 @@ class researcher_signup(CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user, backend='django.contrib.auth.backends.ModelBackend')
-        return redirect('index.html')
+        return redirect('index')
 
 
     
